423847bbcb61eadd82c655be1716d846ca845c22
Yuki Kaito <ykaito21@gmail.com>
2018-05-06 22:09:47 +0900

    address making

--- app.rb
+++ app.rb
@@ -2,11 +2,22 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Hello world!'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
